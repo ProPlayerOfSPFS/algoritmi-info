e9dd28dbde808cd772ff8c770c67525dd9c5cb80
ProPlayerOfSPFS <iustinlacatus11@gmail.com>
2025-10-16 18:26:09 +0300

    Initial commit

---
+++ C++/Lucrat la clasa/algoritm-clasa-25-09-2025.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    float c, d;
+    cout<<"a="; cin>>a;
+    b=10;
+    cout<<"c="; cin>>c;
+    d=2.5;
+    cout<<a+b<<endl;
+    cout<<c+d<<endl;
+    cout<<b/a<<endl;
+    cout<<b%a<<endl;
+    cout<<c*d<<endl;
+    cout<<a*b<<endl;
+    cout<<b-a<<endl;
+    return 0;
+}
